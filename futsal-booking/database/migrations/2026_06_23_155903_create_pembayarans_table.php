f98db34d69699eb6b4a9efe853d28c840a939c1e
Ahmad Mulia Aliansyah <2511501203@student.budiluhur.ac.id>
2026-06-24 05:42:15 +0700

    feat: initialize core database migrations and Eloquent models for booking management system

---
+++ futsal-booking/database/migrations/2026_06_23_155903_create_pembayarans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembayarans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')->constrained()->cascadeOnDelete();
+            $table->integer('nominal_dp');
+            $table->string('bukti_transfer')->nullable();
+            $table->enum('status_verifikasi', ['menunggu', 'disetujui', 'ditolak'])->default('menunggu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayarans');
+    }
+};
